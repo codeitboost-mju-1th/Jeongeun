071b3c55d32c9a642ae0f8c7afd66b269bf5f28b
jenn2i <dkeun8@gmail.com>
2024-08-23 23:26:12 +0900

    실습

--- onebite-react/todoList/src/components/List.jsx
+++ onebite-react/todoList/src/components/List.jsx
@@ -1,6 +1,6 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 const List = ({ todos, onUpdate, onDelete }) => {
   const [search, setSearch] = useState("");
@@ -20,9 +20,27 @@ const List = ({ todos, onUpdate, onDelete }) => {
 
   const filteredTodos = getFilteredData();
 
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todos]);
+  // 의존성배열 : deps
+
   return (
     <div className="List">
       <h4>Todo List 🌱</h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>done: {doneCountCount}</div>
+        <div>notDone: {notDoneCount}</div>
+      </div>
       <input
         value={search}
         onChange={onChangeSearch}
